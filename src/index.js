2a2674937b2290647675bb18388acb59e401f000
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2019-12-15 17:43:37 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import {choice, remove } from './helpers';
+import foods from './foods';
+
+
+// randomly draw a fruit from the array
+let fruit = choice (foods);
+
+// Log the message "I'd like one RANDOMFRUI, please."
+console.log(`I'd like one ${fruit}, please`);
+
+// Log the message "Here you go: RANDOMFRUIT"
+console.log(`Here you go: ${fruit}`);
+
+// Log the message "Delicious! May I have another?"
+console.log(`Delicious! May I have another?`);
+
+// Remove the fruit from the array of fruits
+let remaining = remove(foods, fruit);
+
+// Log the message "I'm sorry, we're all out. We have  FRUITSLEFT left."
+console.log(`I'm sorry, we're all out. We have ${ remaining.length } other types of fruit left. `);
